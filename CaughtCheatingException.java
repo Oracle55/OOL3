f9746d143c3708314747e7ead5f33c766a684f7a
Moses Ilunga <ilunga.moses@outlook.com>
2016-11-12 18:28:40 -0600

    Update CaughtCheatingException.java

--- CaughtCheatingException.java
+++ CaughtCheatingException.java
@@ -1,3 +1,10 @@
+/**
+*
+*@author Moses Ilunga - (Will add what I did)
+*@author Ben Delzer - 
+*@author Christian Hansen -
+*@author Brock -
+*/
 import java.util.*;
 public class CaughtCheatingException extends Exception
 {
