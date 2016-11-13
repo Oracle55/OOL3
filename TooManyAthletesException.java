e102087919c70f0e3940d77e3756927d2ea50f90
Moses Ilunga <ilunga.moses@outlook.com>
2016-11-12 18:31:04 -0600

    Update TooManyAthletesException.java

--- TooManyAthletesException.java
+++ TooManyAthletesException.java
@@ -1,3 +1,11 @@
+/**
+*
+*@author Moses Ilunga - (Will add what I did)
+*@author Ben Delzer - 
+*@author Christian Hansen -
+*@author Brock -
+*@author Tim Dusek -
+*/
 public class TooManyAthletesException extends Exception
 {
      public TooManyAthletesException()
